758efdf880eed8ba7eea01479bdfd76da79fdcce
xr7a <146667996+xr7a@users.noreply.github.com>
2023-10-17 12:09:18 +0300

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,10 @@
+﻿namespace CarWorkshop
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+    }
+}
