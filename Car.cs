0aa79d10cdf074ccebc1a699f626f77b2ac5f0ee
xr7a <ivandesyatov3@gmail.com>
2023-10-18 20:12:01 +0300

    доделал car

--- Car.cs
+++ Car.cs
@@ -2,5 +2,12 @@
 {
     public class Car
     {
+        public int CarId { get; set; }
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public int YearOfRelease { get; set; }
+        public string VinCode { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
     }
 }
