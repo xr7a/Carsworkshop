76045c9eccf253824c5e718af17ec962eeee6f4e
xr7a <146667996+xr7a@users.noreply.github.com>
2023-10-18 19:58:18 +0300

    Create User.cs

---
+++ User.cs
@@ -0,0 +1,12 @@
+namespace CarWorkshop
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Adress { get; set; }
+    }
+}
