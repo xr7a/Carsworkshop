3bac4f0422e0bd12e23cb22d2dc3c4754a7d7927
xr7a <ivandesyatov3@gmail.com>
2023-10-18 20:09:57 +0300

    <Обязательно>

--- Order.cs
+++ Order.cs
@@ -1,6 +1,10 @@
 ﻿namespace CarWorkshop
 {
-    public class Order
+    public class Order : User
     {
+        public int OrderId { get; set; }
+        public DateTime OrderTime { get; set; }
+        public string OrderDescription { get; set; }
+        public string OrderStatus { get; set; }
     }
 }
