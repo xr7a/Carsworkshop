200113047bcb156d7e0f424774efb3cc48919127
xr7a <ivandesyatov3@gmail.com>
2023-10-18 20:40:42 +0300

    написал бд для пользователей

--- Properties/Repositories/OrdersRepository.cs
+++ Properties/Repositories/OrdersRepository.cs
@@ -1,4 +1,6 @@
-﻿namespace CarWorkshop.Properties.Repositories
+﻿using System.Collections.Generic;
+
+namespace CarWorkshop.Properties.Repositories
 {
     public class OrdersRepository
     {
--- Properties/Repositories/UsersRepository.cs
+++ Properties/Repositories/UsersRepository.cs
@@ -1,6 +1,54 @@
-﻿namespace CarWorkshop.Properties.Repositories
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarWorkshop.Properties.Repositories
 {
     public class UsersRepository
     {
+        public static List<User> users = new List<User>
+        {
+            new User
+            {
+                Id = 1,
+                FirstName = "Ivan",
+                LastName = "Ivanov",
+                Email = "fefl;k@yandex.ru",
+                PhoneNumber = "89240241221",
+                Adress = "Saratov, street of Peace, 106"
+            },
+            new User
+            {
+                Id = 2,
+                FirstName = "Yaroslava",
+                LastName = "Petrovna",
+                Adress = "Penza, military town, 16",
+                PhoneNumber = "89024528156"
+            }
+        };
+        public List<User> GetAll()
+        {
+            return users;
+        }
+        public void Add(User user)
+        {
+            users.Add(user);
+        }
+        public void Update(int id, string email, string number, string adress) 
+        {
+            User userToUpdate = users.Find(c => c.Id == id);
+            if(userToUpdate != null) {
+            userToUpdate.Email = email;
+            userToUpdate.PhoneNumber = number;
+            userToUpdate.Adress = adress;
+            }
+        }
+        public void Delete(int id)
+        {
+            User userToDelete = users.Find(c => c.Id == id);
+            if (userToDelete != null)
+            {
+                users.Remove(userToDelete); 
+            }
+
+        }
     }
 }
